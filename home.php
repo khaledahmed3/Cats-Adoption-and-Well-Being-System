badcc5249c84890e1b21c6f805a70f41568a0949
Khalid <khaledahmed512@gmail.com>
2022-04-05 16:53:46 +0800

    added structure folders

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="eng">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <!-- header section starts -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <!-- header section ends -->
+
+
+
+
+
+
+
+
+        <!-- swiper js link -->
+        <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+        <!-- custom js file link -->
+        <script src="js/script.js"></script>
+
+</body>
+
+</html>
