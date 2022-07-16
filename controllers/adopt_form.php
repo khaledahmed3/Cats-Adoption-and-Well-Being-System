16096f9f433daf23a02bc099bc9ab40844e6ca01
jalalajlan <jalalajlan@gmail.com>
2022-07-16 22:35:11 +0800

    edit adopt form line 3

--- controllers/adopt_form.php
+++ controllers/adopt_form.php
@@ -1,6 +1,6 @@
 <?php
 
-$connection = new mysqli('"us-cdbr-east-06.cleardb.net", "b6a98a7accba2a", "85cf840c", "heroku_fbe29bfef8d965d"');
+$connection = new mysqli("us-cdbr-east-06.cleardb.net", "b6a98a7accba2a", "85cf840c", "heroku_fbe29bfef8d965d");
 
 session_start();
 
