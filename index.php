2b07397db98ec615e2ed2deb9182063adcc9c6d4
Khalid <khaledahmed512@gmail.com>
2022-07-16 21:35:04 +0800

    heroku

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 <html lang="eng">
 
 <?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 session_start();
 $username = $_SESSION ? $_SESSION['username'] : null;
 ?>
